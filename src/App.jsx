fc6f2d7e60e4778276e77145acad7d48ad94a632
Yannick <yannickcruz02@gmail.com>
2026-04-15 16:45:03 -0300

    feat: Feito a página inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import StartPage from './components/StartPage'
+import Editor from './components/Editor'
 
 function App() {
   return(
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<StartPage />} />
+          <Route path='editor' element={<Editor />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/Editor.jsx
@@ -0,0 +1,11 @@
+import "../css/Editor.css"
+
+const Editor = () => {
+    return(
+        <section id="Editor">
+            <h1>Editor de CSV</h1>
+        </section>
+    )
+}
+
+export default Editor
