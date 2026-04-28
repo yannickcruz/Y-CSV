5f3152f0f0012fdc907f76da116a27f88339e0ee
Yannick <yannickcruz02@gmail.com>
2026-04-28 16:29:05 -0300

    feat: Tabela dinâmica para exibição de CSVs

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,14 +1,55 @@
 import "../css/Editor.css"
+import { useRef } from "react";
 
-const Editor = () => {
+const Editor = (csv) => {
+
+    // Exemplo de JSON de CSV
+    const csv_example = [
+        {
+            Filename: "image_filename.jpg",
+            Title: "A short description of what the asset represents",
+            Keywords: "Keyword1, Keyword2, Keyword3, Keyword4, Keyword5",
+            Category: 3,
+            Releases: "Haleeq Whitten, Ludovic Hillion, Morgan Greentstreet, Christine Manore"
+        },
+        {
+            Filename: "footage_filename.mov",
+            Title: "Up to 200 characters",
+            Keywords: "Most important keywords first. Max 49 keywords.",
+            Category: "Enter the number matching the category in the upload-CSV dialog",
+            Releases: "The names you gave to the releases when you uploaded them on Adobe Stock"
+        }
+    ];
+
+    const headers = csv_example.length > 0 ? Object.keys(csv_example[0]) : [];
+
+
     return(
         <section id="Editor">
             <header id="editor-header">
                 <h1 id="editor-title">Y-CSV</h1>
             </header>
 
-            <div id="CSV-table">
-                {/*Tabela estática apenas para teste. Ao finalizar, implementar tabela gerada por código*/}
+            <div id="table-container">
+                <table id="csv-table">
+                    <thead>
+                        <tr>
+                            {headers.map((header) => (
+                                <th key={header}>{header}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {csv_example.map((row, index) => (
+                            <tr key={index}>
+                                {headers.map((header) => (
+                                    <td key={header}>{row[header]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                    
             </div>
         </section>
     )
